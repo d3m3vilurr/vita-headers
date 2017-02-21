2dbb6d9eecedd822ed5ba6d48713e5505e651875
Sunguk Lee <d3m3vilurr@gmail.com>
2017-02-21 09:04:31 +0900

    Fix rate limit printing

--- .travis.d/download_sdk.sh
+++ .travis.d/download_sdk.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 cd $TRAVIS_BUILD_DIR
-curl -sL https://$GH_TOKEN@api.github.com/rate_limit | grep remaining | head -n 1
+curl -i https://$GH_TOKEN@api.github.com/rate_limit
 curl https://$GH_TOKEN@api.github.com/repos/vitasdk/autobuilds/releases | grep browser_download_url | grep linux | head -n 1 | cut -d '"' -f 4 | xargs curl -L | tar xj
