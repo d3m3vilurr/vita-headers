367a93053a2b4837c1949c114c35dbfad87fb906
Sunguk Lee <d3m3vilurr@gmail.com>
2017-02-21 20:02:20 +0900

    Parse release page instead API call

--- .travis.d/download_sdk.sh
+++ .travis.d/download_sdk.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 set -e
 cd $TRAVIS_BUILD_DIR
-curl -i https://$GH_TOKEN@api.github.com/rate_limit
-curl https://$GH_TOKEN@api.github.com/repos/vitasdk/autobuilds/releases | grep browser_download_url | grep linux | head -n 1 | cut -d '"' -f 4 | xargs curl -L | tar xj
+curl -sL https://github.com/vitasdk/autobuilds/releases | grep linux | grep tar.bz2 | head -n 1 | cut -d '"' -f 2 | xargs -I PATH curl -L https://github.comPATH | tar xj
